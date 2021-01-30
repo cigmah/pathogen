28267a3a7f96454ab3036677b184ea8c88044999
wjmn <Jamin.Wu07@gmail.com>
2021-01-31 10:09:46 +1100

    Formatting change.

--- src/Components/GenerateButton.tsx
+++ src/Components/GenerateButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Condition } from "../Lib/Condition";
 import { parseConfig } from "../Lib/Generator";
 import { Investigation } from "../Lib/Investigation";
